9aa3f9ced261160456b88e0f407e3fa5b09b0e0d
Karina Matveeva <matveeva664664@mail.ru>
2024-12-05 10:36:43 +0300

    Update task3.c

--- 3824Б1ФИ2/14 Матвеева Карина Александровна/task3.c
+++ 3824Б1ФИ2/14 Матвеева Карина Александровна/task3.c
@@ -1,13 +1,12 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-#include <limits.h>
+
 double max_of_two_numbers(double a, double b)
 {
-  double max;
 if (a > b) {
-    max=a;
+    return a;
 }
 else {
-    max=b;
+    return b;
 }
-  return 0.0;
+  
 }

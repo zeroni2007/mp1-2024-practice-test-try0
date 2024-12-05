b3975ad2b1ab82b9bf6ca09a6b652310214b18b2
Polina0013 <123825611+Polina0013@users.noreply.github.com>
2024-12-05 09:58:42 +0300

    Update task2.c

--- 3824Б1ПР2/18 Позднова Полина Дмитриевна/task2.c
+++ 3824Б1ПР2/18 Позднова Полина Дмитриевна/task2.c
@@ -1,6 +1,11 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <stdio.h>
+#include <limits.h>
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+	int result;
+	result = a * b;
+	if (result >= UINT_MAX) result = UINT_MAX;
+	return result;
 }

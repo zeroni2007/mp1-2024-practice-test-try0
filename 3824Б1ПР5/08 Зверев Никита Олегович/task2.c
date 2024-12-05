6f6a7d2bae5821cf1bd85a707ff1f674ad37f2d3
Зверев Никита Олегович <mrnikitos098@gmail.com>
2024-12-05 14:10:19 +0300

    Update task2.c

--- 3824Б1ПР5/08 Зверев Никита Олегович/task2.c
+++ 3824Б1ПР5/08 Зверев Никита Олегович/task2.c
@@ -1,6 +1,18 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    if (a > 0 && b > UINT_MAX / a) return UINT_MAX;
+    return a * b;
+}
+
+int main() {
+    unsigned int a, b;
+    printf("Введите два натуральных числа (a и b): ");
+    scanf_s("%u %u", &a, &b);
+    unsigned int result = product_of_two_numbers(a, b);
+    printf("Произведение: %u\n", result);
+    return 0;
 }

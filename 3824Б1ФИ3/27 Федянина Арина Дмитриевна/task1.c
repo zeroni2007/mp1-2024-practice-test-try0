3738a14d7ca84000eeedf4fabc19ab0fca3a2414
pupupu <fedyninaarina2000@gmail.com>
2024-12-05 09:52:21 +0300

    Update task1.c

--- 3824Б1ФИ3/27 Федянина Арина Дмитриевна/task1.c
+++ 3824Б1ФИ3/27 Федянина Арина Дмитриевна/task1.c
@@ -3,5 +3,14 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
+	if (((a + b) >= INT_MAX) or ((a + b) <= INT_MIN))
+	{
+		if ((a + b) >= INT_MAX) return INT_MAX;
+		else return INT_MIN;
+	}
+	else
+	{
+		return a + b;
+	}
   return 0;
 }

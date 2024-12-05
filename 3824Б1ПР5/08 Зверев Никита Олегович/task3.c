fb564410d89e7b1fd6d532e1a1543ee0eb03117a
Зверев Никита Олегович <mrnikitos098@gmail.com>
2024-12-05 15:07:58 +0300

    Update task3.c

--- 3824Б1ПР5/08 Зверев Никита Олегович/task3.c
+++ 3824Б1ПР5/08 Зверев Никита Олегович/task3.c
@@ -1,5 +1,18 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+#include <stdio.h>
+#include <locale.h>
+
+double max_of_two_numbers(double a, double b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a, b;
+    printf("Введите a: ");
+    scanf("%lf", &a);
+    printf("Введите b: ");
+    scanf("%lf", &b);
+    printf("Максимум: %lf\n", max_of_two_numbers(a, b));
+    return 0.0;
 }

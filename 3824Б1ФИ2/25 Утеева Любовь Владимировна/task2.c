86dd9228fc167be2ce06b7de7a039ec07d58faa5
Luba Uteeva <lubauteeva@gmail.com>
2024-12-05 10:19:26 +0300

    Update task2.c

--- 3824Б1ФИ2/25 Утеева Любовь Владимировна/task2.c
+++ 3824Б1ФИ2/25 Утеева Любовь Владимировна/task2.c
@@ -11,13 +11,3 @@ unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
         return a * b;
     }
 }
-
-int main() {
-    setlocale(LC_ALL, "Rus");
-    unsigned int a, b;
-    printf("Введите два натуральных числа (a и b): ");
-    scanf_s("%u %u", &a, &b);
-    unsigned int result = product_of_two_numbers(a, b);
-    printf("Произведение a * b = %u\n", result);
-    return 0;
-}

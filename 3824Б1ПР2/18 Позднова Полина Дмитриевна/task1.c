9fbe6da0be7ef20fe2bbe5e03a454ede31f21dc9
Polina0013 <123825611+Polina0013@users.noreply.github.com>
2024-12-05 09:40:49 +0300

    Update task1.c

--- 3824Б1ПР2/18 Позднова Полина Дмитриевна/task1.c
+++ 3824Б1ПР2/18 Позднова Полина Дмитриевна/task1.c
@@ -1,7 +1,14 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+
+#include <stdio.h>
+#include <limits.h>
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+	int result;
+	result = a + b;
+	if (result >= INT_MAX) result = INT_MAX;
+	if (result <= INT_MIN) result = INT_MIN;
+	return result;
 }

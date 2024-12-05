fa3687a80bae1730958010b21042562019ec88e2
zeroni2007 <143108448+zeroni2007@users.noreply.github.com>
2024-12-05 10:45:59 +0300

    Update task1.c

--- 3824Б1ПР6/02 Агаев Амиль Анарович/task1.c
+++ 3824Б1ПР6/02 Агаев Амиль Анарович/task1.c
@@ -3,5 +3,16 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
+
+    if (a > 0 && b > 0 && a > INT_MAX - b) {
+        // Если оба числа положительные и сумма превышает INT_MAX
+        return INT_MAX;
+    }
+    if (a < 0 && b < 0 && a < INT_MIN - b) {
+        // Если оба числа отрицательные и сумма меньше INT_MIN
+        return INT_MIN;
+    }
+    // В остальных случаях безопасно вернуть сумму
+    return a + b;
   return 0;
 }

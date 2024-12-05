897324360b6142b7c3de1d987a30a548fd94ba14
Антон Кондрашкин <821019@bk.ru>
2024-12-05 10:26:50 +0300

    Update task1.c

--- 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task1.c
+++ 3824Б1ФИ1/16 Кондрашкин Антон Алексеевич/task1.c
@@ -3,10 +3,11 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b){ 
   double c = a + b;
-  if (c >= INT_MAX) 
+  if (a >= INT_MAX - b) {
     return INT_MAX;
-  else if (c <= INT_MIN)
+  }
+  if (a <= INT_MIN - b) {
     return INT_MIN;
-  else 
-    return (int)c;
+  }
+  return c;
 }

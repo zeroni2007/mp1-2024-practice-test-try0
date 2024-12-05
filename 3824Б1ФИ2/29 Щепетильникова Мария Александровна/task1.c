f6ae14c77b871a75461cec0c1decff27d1dc4e46
Frestelse <89030541016@mail.ru>
2024-12-05 10:09:53 +0300

    Update task1.c

--- 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task1.c
+++ 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task1.c
@@ -2,14 +2,17 @@
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
 
+#include <limits.h>
+
 int sum_of_two_numbers(int a, int b){
-    if (a >= 0 && b >= 0) {
-        if (a > INT_MAX - b) 
-          return INT_MAX;
-    }
-    else if (a < 0 && b < 0) {
-        if (a < INT_MIN - b) 
-          return INT_MIN;
-    }
+    if (a >= 0 && b >= INT_MAX - a) 
+        return INT_MAX; 
+    else if (b >= 0 && a >= INT_MAX - b) 
+        return INT_MAX; 
+    
+    else if (a <= 0 && b <= INT_MIN - a) 
+        return INT_MIN; 
+    else if (b <= 0 && a <= INT_MIN - b) 
+        return INT_MIN; 
     return a + b;
 }

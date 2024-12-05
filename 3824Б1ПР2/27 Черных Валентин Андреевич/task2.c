9b94576441a58290c5231f423a059bfe1ebe678a
DontelCardem54 <93559646+DontelCardem54@users.noreply.github.com>
2024-12-05 10:29:33 +0300

    Update task2.c

--- 3824Б1ПР2/27 Черных Валентин Андреевич/task2.c
+++ 3824Б1ПР2/27 Черных Валентин Андреевич/task2.c
@@ -2,9 +2,9 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 #include <limits.h>
 
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-    if (a >= UINT_MAX / b)
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b){
+
+    if (b != 0 && a >= UINT_MAX / b)
         return UINT_MAX;
 
     unsigned int result = a * b;

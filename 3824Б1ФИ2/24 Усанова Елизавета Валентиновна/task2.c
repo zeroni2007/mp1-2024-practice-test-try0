616a24d95725e7349415a3e7f46e8babd0fb4173
oduvanchik-108 <1208liza@gmail.com>
2024-12-05 10:17:37 +0300

    Update task2.c

--- 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
+++ 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
@@ -1,7 +1,7 @@
 #include <limits.h>
 #include <stdio.h>
 
-unsigned int proga(unsigned int a, unsigned int b) {
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
     if (a == 0 || b == 0) return 0; 
 
     if (a > UINT_MAX / b) { 

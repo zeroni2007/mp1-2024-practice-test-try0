9af815652a6223f7aeb5b1ac8b8b74c4d4a263ab
delaxeg <de1axeg@gmail.com>
2024-12-05 10:42:14 +0300

    Update task1.c

--- 3824Б1ФИ3/01 Аброян Ваган Папович/task1.c
+++ 3824Б1ФИ3/01 Аброян Ваган Папович/task1.c
@@ -1,12 +1,20 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-double max_of_two_numbers(double a, double b) { 
-  if (a > b) { 
-    return a; 
-  } else if (a < b) { 
-    return b; 
-  } else { 
-    return 0.0; // Возвращаем 0.0, если числа равны 
-  } 
+#include <limits.h>
+
+int sum_of_two_numbers(int a, int b)
+{
+    if (a + b >= INT_MAX)
+    {
+        return INT_MAX;  // Возвращаем INT_MAX, если сумма больше или равна INT_MAX
+    }
+    else if (a + b <= INT_MIN)
+    {
+        return INT_MIN;  // Возвращаем INT_MIN, если сумма меньше или равна INT_MIN
+    }
+    else
+    {
+        return a + b;  // Возвращаем сумму, если она в пределах диапазона
+    }
 }

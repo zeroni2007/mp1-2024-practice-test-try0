f9f87fc02f9f4b28f02cb41d2429ae4d98fed514
NititaTch <136841289+MNikitattt@users.noreply.github.com>
2024-12-05 10:29:02 +0300

    Update task3.c

--- 3824Б1ФИ2/30 Щербаков Никита Андреевич/task3.c
+++ 3824Б1ФИ2/30 Щербаков Никита Андреевич/task3.c
@@ -1,5 +1,34 @@
+#include <limits.h>
+#include <math.h>
 // Задача 3. Найти максимум из вешественных чисел a и b
 double max_of_two_numbers(double a, double b)
 {
-  return 0.0;
+	if (a < 0.0 && b >= 0.0)
+		return b;
+	else if (b < 0.0 && a >= 0.0)
+		return a;
+	else if (fabs(a) > fabs(b) && a <= 0.0 && b <= 0.0)
+	{
+		if (a == 0.0 && b == 0.0)
+			return a;
+		return b;
+	}
+	else if (fabs(a) < fabs(b) && a <= 0.0 && b <= 0.0)
+	{
+		if (a == 0.0 && b == 0.0)
+			return a;
+		return a;
+	}
+	else if (fabs(a) > fabs(b) && a >= 0.0 && b >= 0.0)
+	{
+		if (a == 0.0 && b == 0.0)
+			return a;
+		return a;
+	}
+	else if (fabs(a) < fabs(b) && a >= 0.0 && b >= 0.0)
+	{
+		if (a == 0.0 && b == 0.0)
+			return a;
+		return b;
+	}
 }

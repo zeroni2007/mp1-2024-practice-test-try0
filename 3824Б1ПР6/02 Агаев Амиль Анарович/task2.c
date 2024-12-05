f99c5de24e7b89292ed57290f4e321178b1bab39
zeroni2007 <143108448+zeroni2007@users.noreply.github.com>
2024-12-05 10:50:31 +0300

    Update task2.c

--- 3824Б1ПР6/02 Агаев Амиль Анарович/task2.c
+++ 3824Б1ПР6/02 Агаев Амиль Анарович/task2.c
@@ -1,6 +1,23 @@
-// Задача 2. Найти произведение натуральных чисел a и b
-// Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <iostream>
+#include <limits.h> 
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
+   if (a != 0 && b > UINT_MAX / a) {
+        return UINT_MAX;
+    }
+    return a * b;
+}
+
+int main() {
+    unsigned int a, b;
+
+    // Ввод чисел
+    std::cout << "Введите два числа: ";
+    std::cin >> a >> b;
+
+    unsigned int result = product_of_two_numbers(a, b);
+    std::cout << "Результат: " << result << std::endl;
+
+    return 0;
   return 0;
 }

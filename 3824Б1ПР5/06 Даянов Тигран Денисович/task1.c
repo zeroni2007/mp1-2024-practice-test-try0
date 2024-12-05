dc822ef94d73831dd132c432f5b4769542f4a6b0
Tigran200630 <tigrandayanov2006@gmail.com>
2024-12-05 13:50:46 +0300

    Update task1.c

--- 3824Б1ПР5/06 Даянов Тигран Денисович/task1.c
+++ 3824Б1ПР5/06 Даянов Тигран Денисович/task1.c
@@ -1,7 +1,16 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+
+#include <limits.h>
+
+int sum_safe(int a, int b) {
+    long long sum = a + b;
+    if ((long long)INT_MAX < sum) {
+        return INT_MAX;
+    } else if ((long long)INT_MIN > sum) {
+        return INT_MIN;
+    } else {
+        return (int)sum;
+    }
 }

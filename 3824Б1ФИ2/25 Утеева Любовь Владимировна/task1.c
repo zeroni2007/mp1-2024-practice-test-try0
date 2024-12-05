00e1e4d56f1eb1025faccf801fade611071c3cda
Luba Uteeva <lubauteeva@gmail.com>
2024-12-05 09:33:03 +0300

    Update task1.c

--- 3824Б1ФИ2/25 Утеева Любовь Владимировна/task1.c
+++ 3824Б1ФИ2/25 Утеева Любовь Владимировна/task1.c
@@ -1,7 +1,24 @@
-// Задача 1. Найти сумму целых чисел a и b.
-// Если a + b >= INT_MAX, возвращать INT_MAX
-// Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+#include <locale.h>
+
+int sum_of_two_numbers(int a, int b) {
+    long long sum = (long long)a + b; 
+    if (sum >= INT_MAX) {
+        return INT_MAX;
+    }
+    else if (sum <= INT_MIN) {
+        return INT_MIN;
+    }
+    return (int)sum; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    int a, b, sum;
+    printf("Введите два целых числа: ");
+    scanf_s("%d %d", &a, &b);
+    int result = sum_of_two_numbers(a, b);
+    printf("Сумма: %d\n", result);
+    return 0;
 }

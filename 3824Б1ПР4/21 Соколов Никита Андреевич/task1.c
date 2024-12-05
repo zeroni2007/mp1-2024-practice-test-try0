47f85ba62d369a2862abc3d09412b5af68ca45cc
Nyshotakoe <nikitasokls2000@gmail.com>
2024-12-05 10:29:21 +0300

    Update task1.c

--- 3824Б1ПР4/21 Соколов Никита Андреевич/task1.c
+++ 3824Б1ПР4/21 Соколов Никита Андреевич/task1.c
@@ -3,5 +3,15 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    int result = 0;
+    
+    if (a > 0 && b > INT_MAX - a)
+        result = INT_MAX;
+    else if (a < 0 && b < INT_MIN - a)
+        result = INT_MIN;
+    else
+        result = a + b; \
+    
+    return result;
+    return 0;
 }

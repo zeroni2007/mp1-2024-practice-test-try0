336845e1ff6a140747508739a3ea4635c4440712
posypr09 <aboba228332777@gmail.com>
2024-12-05 10:50:09 +0300

    Update task3.c

--- 3824Б1ПР4/20 Посыпкина Полина Романовна/task3.c
+++ 3824Б1ПР4/20 Посыпкина Полина Романовна/task3.c
@@ -1,5 +1,11 @@
-// Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+#include <stdio.h>
+
+double max_of_two_numbers(double a, double b) {
+    return (a > b) ? a : b;
+}
+
+int main() {
+    printf("Max: %.2f\n", max_of_two_numbers(5.75, 10.25));
+    printf("Max: %.2f\n", max_of_two_numbers(-3.5, -2.8));
+    return 0;
 }

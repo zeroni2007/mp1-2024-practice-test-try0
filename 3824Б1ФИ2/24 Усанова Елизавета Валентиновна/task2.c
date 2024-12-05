ea63f88c502af4b5124008b52a9a26465d83b35e
oduvanchik-108 <1208liza@gmail.com>
2024-12-05 10:15:25 +0300

    Update task2.c

--- 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
+++ 3824Б1ФИ2/24 Усанова Елизавета Валентиновна/task2.c
@@ -1,7 +1,7 @@
 #include <limits.h>
 #include <stdio.h>
 
-unsigned int product_with_overflow_check(unsigned int a, unsigned int b) {
+unsigned int proga(unsigned int a, unsigned int b) {
     if (a == 0 || b == 0) return 0; 
 
     if (a > UINT_MAX / b) { 

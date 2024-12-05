18fa7ed7360c5a13a3779d5c697d7c6dae231dab
Grigoriy Kiryanov <olegmazeratti@yandex.ru>
2024-12-05 09:53:38 +0300

    Update task1.c

--- 3824Б1ФИ3/15 Кирьянов Григорий Александрович/task1.c
+++ 3824Б1ФИ3/15 Кирьянов Григорий Александрович/task1.c
@@ -1,19 +1,20 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
+#include <stdio.h>
 #include <limits.h>
 
-int sum_of_two_numbers(int a, int b) {
-    long long sum = (long long)a + b;
+int sum_of_two_numbers(int a, int b) 
+{ 
+    long long sum = (long long)a + (long long)b; // Используем long long для предотвращения переполнения
 
     if (sum >= INT_MAX) {
-        return INT_MAX;
+        printf("Переполнение: сумма превышает INT_MAX\n");
+    } else if (sum <= INT_MIN) {
+        printf("Переполнение: сумма меньше INT_MIN\n");
+    } else {
+        printf("Результат: %d\n", (int)sum); 
     }
-    else if (sum <= INT_MIN) {
-        return INT_MIN;
-    }
-    else {
-        return (int)sum; // Преобразуем обратно в int
-    }
-}
 
+    return 0; // Возвращаем 0
+}

abd26bd93607874127bd6e09f859618b8f8418a4
Frestelse <89030541016@mail.ru>
2024-12-05 09:48:13 +0300

    Update task3.c

--- 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task3.c
+++ 3824Б1ФИ2/29 Щепетильникова Мария Александровна/task3.c
@@ -1,5 +1,25 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+//Вариант 1.
+#include <math.h> 
+
+double max_of_two_numbers(double a, double b) {
+    if (a == 0) 
+        return b;
+    if (b == 0) 
+        return a;
+    return fmax(a, b);
+}
+
+//Вариант 2.
+double max_of_two_numbers(double a, double b) {
+    if (a == 0) 
+        return b;
+    if (b == 0) 
+        return a;
+    if (a > b) {
+        return a;
+    }
+    else {
+        return b;
+    }
 }

136dd622356c6a1cf419654ede44a778e38e5a7e
loesq <116908536+loesq@users.noreply.github.com>
2024-12-05 14:02:27 +0300

    Update task3.c

--- 3824Б1ПР4/27 Ширяев Иван Александрович/task3.c
+++ 3824Б1ПР4/27 Ширяев Иван Александрович/task3.c
@@ -1,5 +1,15 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b)
-{
-  return 0.0;
+#include <stdio.h> 
+double max_of_two_numbers(double a, double b) 
+{   
+    return (a>b) ? a:b; 
+} 
+ 
+int main(){ 
+ 
+    double a,b; 
+    a = 11.111; 
+    b = 11.111; 
+    printf("%lf",max_of_two_numbers(a,b)); 
+ 
 }

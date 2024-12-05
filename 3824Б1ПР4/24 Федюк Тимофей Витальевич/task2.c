2f5ecfd96e6e64da8b8dcfe7a08fc2ed139db024
FedukTimofej <feduk7777777@gmail.com>
2024-12-05 10:22:36 +0300

    Update task2.c

--- 3824Б1ПР4/24 Федюк Тимофей Витальевич/task2.c
+++ 3824Б1ПР4/24 Федюк Тимофей Витальевич/task2.c
@@ -1,6 +1,19 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
+#include <stdio.h>
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b){
+   if (a * b >= UINT_MAX) {
+    return UINT_MAX;
+  } else {
+    return a * b;
+  }
+}
+int main()
 {
+  unsigned int a, b;
+  printf("two_numbers: ");
+  scanf_s("%d %d", &a, &b);
+  unsigned int product = product_of_two_numbers(a, b);
+  printf("product: %d\n", product);
   return 0;
 }

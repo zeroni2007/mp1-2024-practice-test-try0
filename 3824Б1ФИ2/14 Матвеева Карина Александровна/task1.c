7d0ca93e984c7b6a928271758ceabed711466aa2
Karina Matveeva <matveeva664664@mail.ru>
2024-12-05 10:01:39 +0300

    Update task1.c

--- 3824Б1ФИ2/14 Матвеева Карина Александровна/task1.c
+++ 3824Б1ФИ2/14 Матвеева Карина Александровна/task1.c
@@ -3,5 +3,21 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    if(a >= 0 && INT_MAX - a <= b){
+        return INT_MAX;
+    } 
+    else if (b >= 0 && INT_MAX - b <= a)
+    {
+        return INT_MAX;
+    }
+    else if (a <= 0 && INT_MIN - a >= b)
+    {
+        return INT_MIN;
+    }
+    else if(b <= 0 && INT_MIN - b >= a){
+        return INT_MIN;
+    }
+    
+    return a+b;
 }
+  

f3f2e48a2ee95b377617189f0ccc89c1c0e4d6d3
NititaTch <136841289+MNikitattt@users.noreply.github.com>
2024-12-05 10:17:50 +0300

    Update task2.c

--- 3824Б1ФИ2/30 Щербаков Никита Андреевич/task2.c
+++ 3824Б1ФИ2/30 Щербаков Никита Андреевич/task2.c
@@ -3,7 +3,22 @@
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-	if ((unsigned long long int)a * b >= UINT_MAX)
-		return UINT_MAX;
-	return 0;
+	unsigned int c = UINT_MAX, res = 0;
+	if (a > b)
+	{
+		for (unsigned int i = 1; a * i > a * (i - 1) && i < b + 1; i++)
+			res++;
+		if (res != b)
+			return c;
+	}
+	else if (b > a)
+	{
+		for (unsigned int i = 1; b * i > b * (i - 1) && i < a + 1; i++)
+			res++;
+		if (res != a)
+			return c;
+	}
+	else if (a == 0 || b == 00)
+		return 0;
+	return a * b;
 }

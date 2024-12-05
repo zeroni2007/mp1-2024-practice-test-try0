d02ad7baf97393be16a81253c6fb31ffea4a7648
sinyavinserg <ssinavin2@gmail.com>
2024-12-05 10:24:35 +0300

    Update task1.c

--- 3824Б1ФИ2/21 Синявин Сергей Алексеевич/task1.c
+++ 3824Б1ФИ2/21 Синявин Сергей Алексеевич/task1.c
@@ -1,7 +1,12 @@
 // Задача 1. Найти сумму целых чисел a и b.
 // Если a + b >= INT_MAX, возвращать INT_MAX
 // Если a + b <= INT_MIN, возвращать INT_MIN
-int sum_of_two_numbers(int a, int b)
-{
-  return 0;
+#include <limits.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
+    if (a == 0 || b == 0) {
+        return 0;
+    if ((b > 0) && (a > UINT_MAX / b)) 
+        return UINT_MAX;
+    return a * b;
 }

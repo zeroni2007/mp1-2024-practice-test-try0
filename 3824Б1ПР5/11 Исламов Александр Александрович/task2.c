a25ad2f048b29cfa1b1fb2a74b60e961dcc15d1c
Forelka123 <122482787+Forelka123@users.noreply.github.com>
2024-12-05 10:32:37 +0300

    Update task2.c

--- 3824Б1ПР5/11 Исламов Александр Александрович/task2.c
+++ 3824Б1ПР5/11 Исламов Александр Александрович/task2.c
@@ -1,6 +1,22 @@
 // Задача 2. Найти произведение натуральных чисел a и b
 // Если a * b >= UINT_MAX, возвращать UINT_MAX
+#include <limits.h> 
+#include <stdio.h>  
+#include <locale.h> 
+
 unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
 {
-  return 0;
+    if (a > 0 && b > 0 && a > UINT_MAX / b) {
+        return UINT_MAX; 
+    }
+    return a * b; 
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    unsigned int a = 500; 
+    unsigned int b = 200;
+    unsigned int result = product_of_two_numbers(a, b);
+    printf("Произведение: %u\n", result); 
+    return 0; 
 }

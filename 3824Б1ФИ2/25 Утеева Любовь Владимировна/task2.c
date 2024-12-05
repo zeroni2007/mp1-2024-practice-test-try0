1b8ecebda9fe9f04cd45908a8d91c55458b19fe4
Luba Uteeva <lubauteeva@gmail.com>
2024-12-05 09:54:36 +0300

    Update task2.c

--- 3824Б1ФИ2/25 Утеева Любовь Владимировна/task2.c
+++ 3824Б1ФИ2/25 Утеева Любовь Владимировна/task2.c
@@ -1,6 +1,23 @@
-// Задача 2. Найти произведение натуральных чисел a и b
-// Если a * b >= UINT_MAX, возвращать UINT_MAX
-unsigned int product_of_two_numbers(unsigned int a, unsigned int b)
-{
-  return 0;
+#include <stdio.h>
+#include <limits.h>
+#include <locale.h>
+
+unsigned int product_of_two_numbers(unsigned int a, unsigned int b) {
+    if (a == 0 || b == 0) return 0;
+    if (a > UINT_MAX / b) { 
+        return UINT_MAX;
+    }
+    else {
+        return a * b;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "Rus");
+    unsigned int a, b;
+    printf("Введите два натуральных числа (a и b): ");
+    scanf_s("%u %u", &a, &b);
+    unsigned int result = product_of_two_numbers(a, b);
+    printf("Произведение a * b = %u\n", result);
+    return 0;
 }

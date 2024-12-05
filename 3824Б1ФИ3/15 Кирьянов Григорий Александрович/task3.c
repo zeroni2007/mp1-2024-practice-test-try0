c01bcb8ba51b30d45ff92bbe793e415443cc0a0c
Grigoriy Kiryanov <olegmazeratti@yandex.ru>
2024-12-05 10:30:26 +0300

    Update task3.c

--- 3824Б1ФИ3/15 Кирьянов Григорий Александрович/task3.c
+++ 3824Б1ФИ3/15 Кирьянов Григорий Александрович/task3.c
@@ -1,11 +1,12 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b) 
-{
-  if (a > b) {
-    return a;
-  } else if (a < b) {
-    return b;
-  } else {
-    return 0.0; // Возвращаем 0.0, если числа равны
-  }
+double max_of_two_numbers(double a, double b) {
+    if (a > b) {
+        printf("%lf\n", a); 
+        return a;
+    }
+    else {
+        printf("%lf\n", b); 
+        return b; 
+    }
+    return 0.0;
 }

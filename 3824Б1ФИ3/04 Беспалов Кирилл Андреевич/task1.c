3d9ab604f30aa03633c3422fef5335c03e53d007
Gegegerfjdh <82774893+Gegegerfjdh@users.noreply.github.com>
2024-12-05 10:50:03 +0300

    Update task1.c

--- 3824Б1ФИ3/04 Беспалов Кирилл Андреевич/task1.c
+++ 3824Б1ФИ3/04 Беспалов Кирилл Андреевич/task1.c
@@ -3,5 +3,9 @@
 // Если a + b <= INT_MIN, возвращать INT_MIN
 int sum_of_two_numbers(int a, int b)
 {
-  return 0;
+    
+    if (a+b<INT_MAX && a+b> INT_MIN) {return a+b;}
+    else if (a>0 && b>0 && INT_MAX-b<=a) {return INT_MAX;}
+    else if (a<0 && b<0 && INT_MIN-b>=a) {return INT_MIN;}
+
 }

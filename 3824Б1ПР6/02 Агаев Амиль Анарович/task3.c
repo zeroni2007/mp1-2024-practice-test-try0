dfd552edbdddb79d5221e5cbd567894ad0242a3a
zeroni2007 <143108448+zeroni2007@users.noreply.github.com>
2024-12-09 21:16:21 +0300

    Revert "Update task3.c"

--- 3824Б1ПР6/02 Агаев Амиль Анарович/task3.c
+++ 3824Б1ПР6/02 Агаев Амиль Анарович/task3.c
@@ -1,24 +1,5 @@
 // Задача 3. Найти максимум из вешественных чисел a и b
-double max_of_two_numbers(double a, double b){
- return (a > b) ? a : b;
-}
-
-int main()
+double max_of_two_numbers(double a, double b)
 {
-    double a, b;
-
-    // Ввод двух чисел
-    printf("Введите два вещественных числа: ");
-    scanf("%lf %lf", &a, &b);
-
-    // Нахождение максимума
-    double result = max_of_two_numbers(a, b);
-
-    // Вывод результата
-    printf("Максимум: %.2lf\n", result);
-
-    return 0;
-}
-
   return 0.0;
 }
